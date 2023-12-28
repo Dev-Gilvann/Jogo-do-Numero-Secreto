3fd75edde24617360209d728099447b3d2416457
Manoel Gilvan <gilvannetodv@gmail.com>
2023-12-28 10:54:03 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10
+let numeroLimite = 100
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1
 
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto){
 
 
 exibirTextoNaTela('h1', 'Jogo do número secreto' );
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
